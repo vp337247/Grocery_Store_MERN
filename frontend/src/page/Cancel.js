1e3970c645f966180097076b5923234ed7c07679
vp337247 <vishnu337247@gmail.com>
2023-08-16 12:25:43 +0530

    success and cancel pages updated

--- frontend/src/page/Cancel.js
+++ frontend/src/page/Cancel.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const Cancel = () => {
   return (
-    <div className='bg-red-200 w-full max-w-md m-auto h-36 flex justify-center items-center font-semibold text-lg'>
-        <p>Payment Failed</p>
+    <div className="bg-gray-100 h-screen">
+      <div className="bg-violet p-6 md:mx-auto">
+        
+        <div className="text-center">
+          <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">Payment Failed!</h3>
+          <p className="text-gray-600 my-2">We're sorry, but your payment could not be processed.</p>
+          <p>Please check your payment details and try again.</p>
+          <div className="py-10 text-center">
+            <a href="/" className="px-12 bg-red-600 hover:bg-red-500 text-white font-semibold py-3">
+              GO BACK
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cancel
+export default Cancel;
